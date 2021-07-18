1f5eb01ce818f7b3d66577c3cd785dbc3578a2e8
ed-science <edsg.eds@gmail.com>
2021-07-18 13:44:47 -0300

    insert aws script for bucket sw3

---
+++ MOD09/aws.py
@@ -0,0 +1,35 @@
+#%%
+import boto3
+from botocore import exceptions
+from botocore.exceptions import ClientError
+import logging
+from dotenv import load_dotenv
+from os import getenv
+
+# %%
+load_dotenv('/home/edson/.env')
+
+# %%
+getenv('AWS_ID')
+
+
+# %%
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id = getenv('AWS_ID'),
+    aws_secret_access_key = getenv('AWS_KEY')
+)
+# %%
+
+def criar_bucket(nome):
+    try:
+        s3_client.create_bucket(Bucket=nome)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+# %%
+criar_bucket('ed-s3-bucket-eduser2')
+# %%
+
